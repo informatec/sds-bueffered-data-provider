6ec5dfa4d6426a22f3c18d5240965acd771f0782
pag <goekaypamuk@outlook.com>
2018-07-08 21:19:20 +0200

    additional Buffer

--- index.js
+++ index.js
@@ -26,40 +26,60 @@ const sqlCon = new Promise(function (resolve, reject) {
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         var params = JSON.parse(message)
-
-
-        ws.send(JSON.stringify({
-            id: params.id,
-            data: buffer[params.period][params.aggrLvl][params.measure]
-        }));
-        console.log(params);
+        if ( buffer[params.period] === undefined || buffer[params.period][params.aggrLvl] == undefined || buffer[params.period][params.aggrLvl][params.measure] == undefined ) {
+            fillBuffer({PARAM_MEASURE: params.measure ,PARAM_AGGR: params.aggrLvl, PARAM_PERIOD: params.period})
+                .then(function() {
+                    ws.send(JSON.stringify({
+                        id: params.id,
+                        data: buffer[params.period][params.aggrLvl][params.measure]
+                    }));
+                });
+        } else {
+            ws.send(JSON.stringify({
+                id: params.id,
+                data: buffer[params.period][params.aggrLvl][params.measure]
+            }));
+        }
     });
 
     // ws.send('something');
 });
 
+var progrress = 0;
+var totalProgress = 0;
 
 var metaQuery = 'SELECT [PARAM_PERIOD],[PARAM_AGGR],[PARAM_MEASURE] FROM [sds].[dbo].[META_BUFFER_DEFINITION]'
 query(metaQuery).then(function(result) {
+    console.log(new Date(), 'Start Buffering!');
+    console.log(new Date(), 'Progress 0.0% of 100%');
+
+    totalProgress = result.length;
     result.forEach(function(row) {
-        const cb = row;
-        var innerSql = 'SELECT [UNIXTS],[VALUE] FROM FCT_' + row.PARAM_MEASURE + '_' + row.PARAM_AGGR + '_' + row.PARAM_PERIOD + ' order by 1';
-        query(innerSql).then(function(innerResult) {
-            const arr = [];
-            innerResult.forEach(function(record) {
-                arr.push([record.UNIXTS , record.VALUE]);
-            })
-            bufferData(arr, cb);
-        }).catch(function(err) {
-            console.log(err);
-        });
+        fillBuffer(row);
     })
 
 }).catch(function(err) {
     console.log(err);
 });
 
+function fillBuffer(record) {
+    return new Promise(function(resolve, reject) {
+        var innerSql = 'SELECT [UNIXTS],[VALUE] FROM FCT_' + record.PARAM_MEASURE + '_' + record.PARAM_AGGR + '_' + record.PARAM_PERIOD + ' order by 1';
+        query(innerSql).then(function(innerResult) {
+            const arr = [];
+            innerResult.forEach(function(record) {
+                arr.push([+record.UNIXTS , record.VALUE]);
+            });
+            bufferData(arr, record);
+            progrress++;
+            console.log(new Date(), 'Progress '+ ((progrress / totalProgress) * 100).toFixed(1) + '% of 100%');
 
+            resolve(true);
+        }).catch(function(err) {
+            console.log(err);
+        });
+    });
+}
 
 function query(qu) {
     return new Promise(function(resolve, reject) {
@@ -85,3 +105,8 @@ function bufferData(data, def) {
     buffer[def.PARAM_PERIOD][def.PARAM_AGGR][def.PARAM_MEASURE] = data;
 }
 
+
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    console.log("Prevent node exiting...");
+});
